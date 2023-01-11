4b3ad2caddea937f0716b82f22defabd64705d53
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-11 15:49:39 +0530

    Create User.java

---
+++ src/main/java/com/ecom/webapp/secure/User.java
@@ -0,0 +1,61 @@
+package com.ecom.webapp.secure;
+
+import com.ecom.webapp.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="user_info")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long userId;
+	
+	@Column(unique = true)
+	@NotBlank(message = "User Name cannot be empty")
+	private String username;
+	
+	@NotBlank(message = "Password cannot be empty")
+	@JsonIgnore
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+}

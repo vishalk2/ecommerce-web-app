23519b14d95775e0de872e971f29cf1c37c8a2aa
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-11 15:12:25 +0530

    Update Admin.java

--- src/main/java/com/ecom/webapp/entity/Admin.java
+++ src/main/java/com/ecom/webapp/entity/Admin.java
@@ -1,7 +1,60 @@
 package com.ecom.webapp.entity;
 
+import com.ecom.webapp.secure.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "admin_info")
 public class Admin extends User {
+	
+	@NotBlank(message = "Admin Name cannot be empty!")
 	private String name;
+	
+	@NotBlank(message = "Email Id cannot be empty!")
+	@Email(message = "Not a proper Email ID format! Enter again.")
 	private String mailId;
+	
+	@NotBlank(message = "Phone Number cannot be empty!")
+	@Size(min = 10, max = 10, message = "Invalid Phone Number please enter a vaild phone number minimum of 10 digits")
+	@Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only")
+	@Column(unique = true)
 	private String phoneNo;
+	
+	public Admin() {
+	}
+	
+	public Admin(@NotBlank(message = "Admin Name cannot be empty!") String name,
+			@NotBlank(message = "Email Id cannot be empty!") @Email(message = "Not a proper Email ID format! Enter again.") String mailId,
+			@NotBlank(message = "Phone Number cannot be empty!") @Size(min = 10, max = 10, message = "Invalid Phone Number please enter a vaild phone number minimum of 10 digits") @Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only") String phoneNo) {
+		super();
+		this.name = name;
+		this.mailId = mailId;
+		this.phoneNo = phoneNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
 }

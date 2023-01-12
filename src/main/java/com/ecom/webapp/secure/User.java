67cfaef08790dbd8ef93a8b490c09f72f821cc0a
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-12 15:36:02 +0530

    Update User.java

--- src/main/java/com/ecom/webapp/secure/User.java
+++ src/main/java/com/ecom/webapp/secure/User.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -20,7 +22,8 @@ import jakarta.validation.constraints.NotBlank;
 public class User {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+	@SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", initialValue = 1, allocationSize = 1)
 	private long userId;
 	
 	@Column(unique = true)

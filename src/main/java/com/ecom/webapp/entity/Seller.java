12d3d60e733f7ca7bf8a8aa3d00f6372a1bba67c
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-13 00:13:50 +0530

    Update Seller.java

--- src/main/java/com/ecom/webapp/entity/Seller.java
+++ src/main/java/com/ecom/webapp/entity/Seller.java
@@ -2,13 +2,12 @@ package com.ecom.webapp.entity;
 
 import java.util.List;
 
-import com.ecom.webapp.secure.User;
+import com.ecom.webapp.security.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -29,16 +28,16 @@ public class Seller extends User {
 	private String mailId;
 	
 	@NotBlank(message = "Phone Number cannot be empty!")
-	@Size(min = 10, max = 10, message = "Invalid Phone Number please enter a vaild phone number minimum of 10 digits")
-	@Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only")
+	@Size(min = 10, max = 10, message = "Invalid Phone Number. Please enter a vaild phone number.")
+	@Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only.")
 	@Column(unique = true)
 	private String phoneNo;
 	
 	@Embedded
 	private Address address;
 	
-	@OneToMany(fetch = FetchType.EAGER, mappedBy="seller")
-	@JsonManagedReference
+	@OneToMany(mappedBy = "seller")
+	@JsonManagedReference("products")
 	private List<Product> products;
 	
 	public Seller() {
@@ -47,7 +46,7 @@ public class Seller extends User {
 	public Seller(@NotBlank(message = "Name cannot be empty!") String name,
 			@NotBlank(message = "PAN Number cannot be empty!") String pan,
 			@NotBlank(message = "Email Id cannot be empty!") @Email(message = "Not a proper Email ID format! Enter again.") String mailId,
-			@NotBlank(message = "Phone Number cannot be empty!") @Size(min = 10, max = 10, message = "Invalid Phone Number please enter a vaild phone number minimum of 10 digits") @Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only") String phoneNo,
+			@NotBlank(message = "Phone Number cannot be empty!") @Size(min = 10, max = 10, message = "Invalid Phone Number. Please enter a vaild phone number.") @Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only.") String phoneNo,
 			Address address, List<Product> products) {
 		super();
 		this.name = name;

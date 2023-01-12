2ff5975ad7b9d846cfe36fe00e235cd9ec168c0d
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-13 00:11:42 +0530

    Update Category.java

--- src/main/java/com/ecom/webapp/entity/Category.java
+++ src/main/java/com/ecom/webapp/entity/Category.java
@@ -2,47 +2,62 @@ package com.ecom.webapp.entity;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Category {
+	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_seq")
+	@SequenceGenerator(name = "category_id_seq", sequenceName = "category_id_seq", initialValue = 1, allocationSize = 1)
 	private long categoryId;
+	
+	@NotBlank(message = "Category Name cannot be empty!")
 	private String categoryName;
-	@OneToMany(fetch = FetchType.EAGER, mappedBy="category")
-	@JsonManagedReference
+	
+	@OneToMany(mappedBy="category")
+	@JsonManagedReference("productlist")
 	private List<Product> productList;
-	public Category(long categoryId, String categoryName, List<Product> productList) {
+	
+	public Category() {
+	}
+
+	public Category(@NotBlank(message = "Category Name cannot be empty!") String categoryName,
+			List<Product> productList) {
 		super();
-		this.categoryId = categoryId;
 		this.categoryName = categoryName;
 		this.productList = productList;
 	}
+
 	public long getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(long categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getCategoryName() {
 		return categoryName;
 	}
+
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
+
 	public List<Product> getProductList() {
 		return productList;
 	}
+
 	public void setProductList(List<Product> productList) {
 		this.productList = productList;
 	}
-	
 }

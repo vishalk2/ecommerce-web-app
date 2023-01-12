7e0e7c6275ed04de834b134697c28a12886bfcda
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2023-01-13 00:13:07 +0530

    Update Product.java

--- src/main/java/com/ecom/webapp/entity/Product.java
+++ src/main/java/com/ecom/webapp/entity/Product.java
@@ -1,38 +1,53 @@
 package com.ecom.webapp.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.FetchType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+	@SequenceGenerator(name = "product_id_seq", sequenceName = "product_id_seq", initialValue = 1, allocationSize = 1)
 	private long productId;
+	
+	@NotBlank(message = "Product Name cannot be empty!")
 	private String productName;
+	
 	private double weight;
+	
+	@NotBlank(message = "Product Description cannot be empty!")
 	private String description;
-	//thumbnail
-	//images
+	
 	private double price;
-	@ManyToOne(optional=false)
+	
+	@ManyToOne
 	@JoinColumn(name = "CATEGORY_ID")
-	@JsonBackReference
+	@JsonBackReference("productlist")
 	private Category category;
+	
 	private boolean stock;
-	@ManyToOne(optional=false)
+	
+	@ManyToOne
 	@JoinColumn(name = "USER_ID")
-	@JsonBackReference
+	@JsonBackReference("products")
 	private Seller seller;
 	
 	public Product() {
-		super();
 	}
-	public Product(long productId, String productName, double weight, String description, double price,
+	
+	public Product(@NotBlank(message = "Product Name cannot be empty!") String productName, double weight,
+			@NotBlank(message = "Product Description cannot be empty!") String description, double price,
 			Category category, boolean stock, Seller seller) {
 		super();
-		this.productId = productId;
 		this.productName = productName;
 		this.weight = weight;
 		this.description = description;
@@ -41,55 +56,68 @@ public class Product {
 		this.stock = stock;
 		this.seller = seller;
 	}
+
 	public long getProductId() {
 		return productId;
 	}
+
 	public void setProductId(long productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getWeight() {
 		return weight;
 	}
+
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
-	public Seller getSeller() {
-		return seller;
-	}
-	public void setSeller(Seller seller) {
-		this.seller = seller;
-	}
+
 	public boolean isStock() {
 		return stock;
 	}
+
 	public void setStock(boolean stock) {
 		this.stock = stock;
 	}
-	
-	
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
 }

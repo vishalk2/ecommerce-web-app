05ec855483d8aab7255ab19f8a714994095469c8
gopa20 <115770232+gopa20@users.noreply.github.com>
2023-01-12 15:47:19 +0530

    Update Customer.java

--- src/main/java/com/ecom/webapp/entity/Customer.java
+++ src/main/java/com/ecom/webapp/entity/Customer.java
@@ -2,11 +2,13 @@ package com.ecom.webapp.entity;
 
 import java.util.List;
 
-import com.ecom.webapp.security.User;
+import com.ecom.webapp.secure.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
@@ -33,7 +35,8 @@ public class Customer extends User {
 	@Embedded
 	private Address address;
 	
-	@OneToMany
+	@OneToMany(mappedBy="customer")
+	@JsonManagedReference
 	private List<Order> orderList;
 	
 	@OneToOne
